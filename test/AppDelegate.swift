53cd149b87a9d9c8f4692fba36f778be25c41678
John Griffiths <john@uxgent.co>
2016-08-23 09:48:42 +0100

    init

---
+++ test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by John Griffiths on 2016-08-23.
+//  Copyright © 2016 John Griffiths. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by John Griffiths on 2016-08-23.
+//  Copyright © 2016 John Griffiths. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
