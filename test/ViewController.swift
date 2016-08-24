32d9bc349cb2210dc556fd7f492721632d6645ad
John Griffiths <john@uxgent.co>
2016-08-24 06:30:49 +0100

    tab order working!

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,14 +8,29 @@
 
 import Cocoa
 
+class MyWindow: NSWindow {
+    override func recalculateKeyViewLoop() {
+        // Remove. nextKeyView and makeFirstResponder seemed broken with this
+    }
+}
+
 class ViewController: NSViewController {
 
+    @IBOutlet weak var txtA: NSTextField!
+    @IBOutlet weak var txtC: NSTextField!
+    @IBOutlet weak var txtB: NSTextField!
+    @IBOutlet weak var txtD: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //txtA.nextKeyView = txtB
     }
 
+    override func viewWillAppear() {
+        self.view.window?.makeFirstResponder(txtA)
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
